f550ef431be0504bd68b8de643b91cca6adf6122
Aman Pandey <amanpandey231217@gmail.com>
2022-02-27 21:57:46 +0530

    commit2

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SidemenuComponent } from './sidemenu/sidemenu.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { DisplayareaComponent } from './displayarea/displayarea.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SidemenuComponent,
+    NavbarComponent,
+    DisplayareaComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+ 
